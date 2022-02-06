78c0edb73ff42bd66caa400a147ef482b342f888
swanserquack <75944176+swanserquack@users.noreply.github.com>
2022-02-06 15:43:46 +0000

    Got A Working Dark Mode

--- darkmode.js
+++ darkmode.js
@@ -10,3 +10,18 @@ document.documentElement.classList.toggle(
   'dark',
   localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
 );
+
+function SwitchButtons(buttonId) {
+  var hideBtn, showBtn;
+  if (buttonId == 'darkmode') {
+    showBtn = 'lightmode';
+    hideBtn = 'darkmode';
+  } else {
+    showBtn = 'darkmode';
+    hideBtn = 'lightmode';
+  }
+  document.getElementById(hideBtn).style.display = 'none'; //step 2 :additional feature hide button
+  document.getElementById(showBtn).style.display = ''; //step 3:additional feature show button
+
+
+}
